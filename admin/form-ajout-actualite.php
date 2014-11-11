3f86cd28cb9523e9ff62ea1cb432a141eee7ecaa
AxelBaron <axelbaron@outlook.com>
2014-11-11 14:22:39 -0500

    Page ajout actualité fonctionnel.
    Version 1. Des améliorations restent à faire notamment des
    vérifications de caractères et des requêtes préparés.

---
+++ admin/form-ajout-actualite.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Formulaire d'ajout d'Actualités</title>
+		<meta charset="UTF-8"/>
+	</head>
+	
+
+	<body>
+	
+	<h1>Administration #TIM Matane</h1>	
+	<h2>Ajouter une actualité</h2>
+
+	<form action="traitement-ajouter-actualité.php" method="POST">
+	
+		<fieldset>
+			<legend>Formulaire</legend>
+			
+			<div>
+				<label for="titre">Titre</label>
+				<input type="text" name="titre"/>
+			</div>
+			
+			<div>
+				<label for="auteur">Auteur</label>
+				<input type="text" name="auteur" />
+			</div>
+			
+			<div>
+				<label for="date">Date</label>
+				<input type="date" name="date" />
+			</div>
+			
+			<div>
+				<label for="image-preview">Image de preview</label>
+				<input type="text" name="image-preview" />
+			</div>
+			
+			<div>
+				<label for="contenu">Contenu</label>
+				<input type="text" name="contenu" />
+			</div>
+		</fieldset>
+		
+	
+		<input type="submit" name="enregistrer" id="enregistrer" value="Enregistrer"/>
+	</form>
+	
+	</body>
+</html>
