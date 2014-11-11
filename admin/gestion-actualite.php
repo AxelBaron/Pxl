c95c6ea91c5ec432ff9a61d0f2ef0f2ed9334264
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-11-11 16:11:44 -0500

    Page gestion-actualité
    Page terminé!

--- admin/gestion-actualite.php
+++ admin/gestion-actualite.php
@@ -5,6 +5,40 @@
 	<title></title>
 </head>
 <body>
+
+<h1>Actualités</h2>
+<?php
+	//Boucle pour calculer le nombre pour la case tout cocher
+	$sql = "SELECT * FROM actualite";
+		$resultat = $pdo->query($sql);
+		$nbEntre = 0;
+		while($donnees = $resultat->fetch()){
+			$nbEntre++;
+		}
+?>
+
+<!-- Cocher, ajouter et supprimer -->
+<input type='checkbox' id="checkbox-tout" onclick="javascript:checkAndUnCheckAll(<?php echo ($nbEntre); ?>)" />
+<label>Tout cocher/décocher</label>
+<input type='button' value='Ajouter' />
+<input type='button' value='Supprimer' />
+
+<!-- Liste des Actualités -->
+<form>
+	<?php
+		$sql = "SELECT * FROM actualite";
+		$resultat = $pdo->query($sql);
+		$nbEntre = 0;
+		while($donnees = $resultat->fetch()){
+			$nbEntre++;
+			echo("<div>");
+			echo("<input type='checkbox' id='$nbEntre' />");
+			echo($donnees["titre"]);
+			echo("<input type='button' value='Modifier' />");
+			echo("</div>");
+		}
+	?>
 	
+</form>
 </body>
 </html>
