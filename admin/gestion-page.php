bc46cb0731622ac2f1b09dbd7c59019c41d9c4bf
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-11-11 16:24:06 -0500

    Page gestion-page terminer
    ...

--- admin/gestion-page.php
+++ admin/gestion-page.php
@@ -5,6 +5,40 @@
 	<title></title>
 </head>
 <body>
+
+<h1>Page</h2>
+<?php
+	//Boucle pour calculer le nombre pour la case tout cocher
+	$sql = "SELECT * FROM page";
+		$resultat = $pdo->query($sql);
+		$nbEntre = 0;
+		while($donnees = $resultat->fetch()){
+			$nbEntre++;
+		}
+?>
+
+<!-- Cocher, ajouter et supprimer -->
+<input type='checkbox' id="checkbox-tout" onclick="javascript:checkAndUnCheckAll(<?php echo ($nbEntre); ?>)" />
+<label>Tout cocher/décocher</label>
+<input type='button' value='Ajouter' />
+<input type='button' value='Supprimer' />
+
+<!-- Liste des Page -->
+<form>
+	<?php
+		$sql = "SELECT * FROM page";
+		$resultat = $pdo->query($sql);
+		$nbEntre = 0;
+		while($donnees = $resultat->fetch()){
+			$nbEntre++;
+			echo("<div>");
+			echo("<input type='checkbox' id='$nbEntre' />");
+			echo($donnees["titre"]);
+			echo("<input type='button' value='Modifier' />");
+			echo("</div>");
+		}
+	?>
 	
+</form>
 </body>
 </html>
