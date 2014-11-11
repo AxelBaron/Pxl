923f17a9dd4962c56d4549e4d6333df0cfb11474
TrueMisterX <truemisterisation@hotmail.com>
2014-11-11 17:06:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,117 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" dir="ltr">
 <head>
-	<meta charset="UTF-8">
-	<title></title>
+<title>Site Web Techniques d'intégration Multimédia</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="styles/layout.css" type="text/css">
 </head>
-
 <body>
-	
+<div>
+  <header id="header">
+    <nav>
+      <ul>
+         <div id="logo">
+         <a href="#"><img src="images/logo2.png" alt="Logo TIM"></a>
+         </div>
+        <li><a href="#">Actualités</a></li>
+        <li><a href="#">Programme</a></li>
+        <li><a href="#">Réalisations </a></li>
+        <li><a href="#">Cégep</a></li>
+        <li><a href="#">Étudier à Matane</a></li>
+        <li><a href="#">Stages</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+<!-- content -->
+<div class="wrapper row2">
+  <div id="container">
+    <!-- Silder -->
+    <section id="slider"><a href="#"><img src="images/slider.jpg" alt="Slider1"></a></section>
+    <!-- main content -->
+    <div id="homepage">
+     <!-- #Index Image -->
+      <section id="services" class="clear"> 
+          <img src="images/index.png">
+      </section>
+   <!-- #TIM Matane en Vidéo -->
+      <section id="tim_video">
+        <article>
+          <figure>
+            <ul>
+              <li> <h2>#TIM Matane en vidéo </h2> </li>
+              <li> <p>asdfsdgdfjkhgfh,zxmcvnxcnvxcnvxcvnsdnffnsdfnsddgdfgdgdfgertertsdfsf</p></li>
+            </ul>
+          </figure>
+        </article>
+      </section>
+      <div id ="multi_info">
+        Multi c'est quoi
+      </div>
+    </div>
+    <!-- main content -->
+    <div id="content">
+      <section>
+        <ul>
+          <li>
+            <article>
+              
+                  <h2>Actualités</h2>
+                  <hr>
+                  <h3> Titre 1 </h3>
+                  <h5> Dates , heures </h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit libero, rutrum quis ex tincidunt, tincidunt blandit elit. Sed cursus turpis ac urna consectetur, eget gravida lectus luctus. Sed tincidunt eros in sem varius fermentum. Integer eget massa eget ligula dignissim malesuada. Integer aliquam est quis molestie auctor. Suspendisse suscipit felis in velit congue, ac mollis enim dictum. Fusce nisl est, cursus non lectus malesuada, ultrices vestibulum ipsum.</a>.</p>
+                   <img src="images/actualites1.jpg">
+                   <a href="#"> <img src="images/savoir_plus.jpg"> </a>
+                  
+              
+           
+            </article>
+          </li>
+          <li >
+            <article >
+                
+                  <h3>Titre 2 </h3>
+                  <h5> Dates , heures </h5>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit libero, rutrum quis ex tincidunt, tincidunt blandit elit. Sed cursus turpis ac urna consectetur, eget gravida lectus luctus. Sed tincidunt eros in sem varius fermentum. Integer eget massa eget ligula dignissim malesuada. Integer aliquam est quis molestie auctor. Suspendisse suscipit felis in velit congue, ac mollis enim dictum. Fusce nisl est, cursus non lectus malesuada, ultrices vestibulum ipsum.</a>.</p>
+                 <img src="images/actualites1.jpg">
+                 <a href="#"> <img src="images/savoir_plus.jpg"> </a>
+                 <a href="#" id="formation"> <img src="images/formation_avenir.jpg"> </a>
+                
+            </article>
+                 
+          </li>
+        </ul>
+
+      </section>
+
+
+    </div>
+    <!-- right column -->
+    <aside id="right_column">
+      <h2 class="title">Portefolio</h2>
+      <hr>
+      <nav>
+        <ul>
+          <li><a href="#"> <img src="images/proposition_stage.jpg"> </a></li>
+          <li><a href="#"> <img src="images/portefolio_etudiant.jpg"> </a></li>
+          <li><a href="#"> <img src="images/etudiants.jpg"> </a></li>
+          <li><a href="#"> <img src="images/portefolio1.jpg"> </a></li>
+        </ul>
+
+    
+      </nav>
+      <!-- /nav -->
+    </aside>
+    <!-- / content body -->
+  </div>
+</div>
+  <!-- Footer -->
+      <div>
+      <footer >
+      <p>Copyright &copy; 2014 - Tout droits réservé à Technique d'intégration multimédia Matane </p>
+     </footer>
+</div>
 </body>
 </html>
