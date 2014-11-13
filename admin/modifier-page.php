f1ffe69842855bd4cfaa5e5d99af113afc9f6b7c
AxelBaron <axelbaron@outlook.com>
2014-11-13 14:51:27 -0500

    CSS pages modifier
    …

--- admin/modifier-page.php
+++ admin/modifier-page.php
@@ -2,9 +2,19 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="style_admin.css">
+	<title>#TIM Matane - Administration principale</title>
 </head>
 <body>
-	
+
+	<?php include("menu-admin.php");?>
+
+	<div class="right">
+
+		<section>
+
+		</section>
+	</div>
+
 </body>
 </html>
