9f44eee72e62273dc836914c73ef6ba59a8df95b
AxelBaron <axelbaron@outlook.com>
2014-11-14 13:51:47 -0500

    Modi menu admin
    …

--- admin/menu-admin.php
+++ admin/menu-admin.php
@@ -1,9 +1,11 @@
 <nav>
-		<ul>
+		<ul>	
+				<li><a href="gestionprincipal.php">Accueil Administration</a></li>
 				<li><a href="gestion-actualite.php">Actualités</a></li>
 				<li><a href="gestion-portfolio.php">Portfolio</a></li>
 				<li><a href="gestion-menu.php">Menus</a></li>
 				<li><a href="gestion-page.php">Pages</a></li>
 				<li><a href="gestion-admin.php">Administrateurs</a></li>
+				<li><a href="../index.php">Retour page index publique</a></li>
 		</ul>
 	</nav>
