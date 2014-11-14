70c3b7c31f71c001678bbbc1baf43aba0f0a5bbe
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-14 12:46:52 -0500

    Ajout des polices, modif timc'estquoi et css

--- timcestquoi.php
+++ timcestquoi.php
@@ -33,26 +33,25 @@
     <div id="homepage">
      <!-- #Index Image -->
       <section id="services" class="clear"> 
-          <img src="images/index.png">
+          <img src="images/timhr.png" alt="tim?" title="tim?" width="960" height="104">
       </section>
    <!-- #TIM Matane en Vidéo -->
-      <section id="tim_video">
-        <article>
-          <figure>
-            <ul>
-              <li> <h2>#TIM C'est quoi? </h2> </li>
+      <section>
+		<article id="proweb">
+				<img src="images/prosduweb.jpg" alt="Les pros du web" title="Les pros du web" width="412" height="400">
+		</article>
+        <article id="descriptionproweb">
+            
+              <h2>#TIM C'est quoi? </h2>
               <li> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
               Integer non dapibus ligula. Morbi finibus tortor sit amet ex maximus, sed elementum sapien sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Vivamus dapibus finibus ante, vitae tempus velit aliquet sed. Morbi rhoncus lorem quis felis mattis, vitae tincidunt quam malesuada. 
               Aenean tincidunt turpis non arcu elementum varius. In quis tortor sit amet nulla consectetur malesuada in id nulla. 
               Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. </p></li>
-            </ul>
-          </figure>
+            
         </article>
       </section>
-      <div id ="multi_info">
-        Multi c'est quoi
-      </div>
+
     </div>
     <!-- main content -->
     <div id="content">
@@ -112,7 +111,7 @@
   </div>
 </div>
   <!-- Footer -->
-      <div>
+      <div class="clear">
       <footer >
       <p>Copyright &copy; 2014 - Tout droits réservé à Technique d'intégration multimédia Matane </p>
      </footer>
