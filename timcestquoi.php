3fe3d7a76e6bee8a6f09d3e8d047b903626467bf
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-14 14:25:34 -0500

    Include
    Retrait de la partie header des pages, qu’il faudra appeler avec <? php
    include(header.php);?>

--- timcestquoi.php
+++ timcestquoi.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-<title>Site Web Techniques d'intégration Multimédia - Page Tim c'est quoi?</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css" type="text/css">
-</head>
-<body>
-<div>
-  <header id="header">
-    <nav>
-    <!-- Retrait de la div logo de la liste pour un placement plus facile -->
-    <div id="logo">
-         <a href="#"><img src="images/TIMlogo2.svg" alt="Logo TIM" width="240" height="88"></a>
-         </div>
-      <ul>
-        <li><a href="#">Actualités</a></li>
-        <li><a href="#">Programme</a></li>
-        <li><a href="#">Réalisations </a></li>
-        <li><a href="#">Cégep</a></li>
-        <li><a href="#">Étudier à Matane</a></li>
-        <li><a href="#">Stages</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
-  </header>
-</div>
+<? include("header.php");?>
+
 <!-- content -->
 <div class="wrapper row2">
   <div id="container">
