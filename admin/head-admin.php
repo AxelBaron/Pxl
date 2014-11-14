6f6cdeda439888d1b31d691f9d85f951dae2f62a
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-11-14 14:13:04 -0500

    Update and rename head-admin.php to header-admin.php

--- admin/head-admin.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="style_admin.css">
-	<title>#TIM Matane - Administration principale</title>
-</head>
-<body>
