427811385c2de63bfa46f57aa9769395e195bb24
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-14 14:52:36 -0500

    Dossier morceaux
    Dossier morceaux pour contenir les bouts de pages qu’on include.
    Partie « cinq points » partiellement faite (omg)

--- timcestquoi.php
+++ timcestquoi.php
@@ -1,17 +1,17 @@
-<? include("header.php");?>
+<? include("morceaux/header.php");?>
 
 <!-- content -->
 <div class="wrapper row2">
   <div id="container">
-    <!-- Silder -->
+    <!-- Slider -->
     <section id="slider"><a href="#"><img src="images/slider.jpg" alt="Slider1" width="960" height="auto"></a></section>
     <!-- main content -->
     <div id="homepage">
-     <!-- #Index Image -->
+     <!-- #tim? Image -->
       <section id="services" class="clear"> 
           <img src="images/timhr.png" alt="tim?" title="tim?" width="960" height="104">
       </section>
-   <!-- #TIM Matane en Vidéo -->
+   <!-- #TIM? image -->
       <section>
 		<article id="proweb">
 				<img src="images/prosduweb.jpg" alt="Les pros du web" title="Les pros du web" width="412" height="400">
@@ -35,60 +35,11 @@
     <!-- main content -->
     <div id="content">
       <section>
-        <ul>
-          <li>
-            <article>
-              
-                  <h2>Actualités</h2>
-                  <hr>
-                  <h3> Titre 1 </h3>
-                  <h5> Dates , heures </h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit libero, rutrum quis ex tincidunt, tincidunt blandit elit. Sed cursus turpis ac urna consectetur, eget gravida lectus luctus. Sed tincidunt eros in sem varius fermentum. Integer eget massa eget ligula dignissim malesuada. Integer aliquam est quis molestie auctor. Suspendisse suscipit felis in velit congue, ac mollis enim dictum. Fusce nisl est, cursus non lectus malesuada, ultrices vestibulum ipsum.</a>.</p>
-                   <img src="images/actualites1.jpg">
-                   <a href="#"> <img src="images/savoir_plus.jpg"> </a>
-                  
-              
-           
-            </article>
-          </li>
-          <li >
-            <article >
-                
-                  <h3>Titre 2 </h3>
-                  <h5> Dates , heures </h5>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit libero, rutrum quis ex tincidunt, tincidunt blandit elit. Sed cursus turpis ac urna consectetur, eget gravida lectus luctus. Sed tincidunt eros in sem varius fermentum. Integer eget massa eget ligula dignissim malesuada. Integer aliquam est quis molestie auctor. Suspendisse suscipit felis in velit congue, ac mollis enim dictum. Fusce nisl est, cursus non lectus malesuada, ultrices vestibulum ipsum.</a>.</p>
-                 <img src="images/actualites1.jpg">
-                 <a href="#"> <img src="images/savoir_plus.jpg"> </a>
-                 <a href="#" id="formation"> <img src="images/formation_avenir.jpg"> </a>
-                
-            </article>
-                 
-          </li>
-        </ul>
+        <?php include("morceaux/cinqpts.php") ?>
 
+        
       </section>
-
-
-    </div>
-    <!-- right column -->
-    <aside id="right_column">
-      <h2 class="title">Portefolio</h2>
-      <hr>
-      <nav>
-        <ul>
-          <li><a href="#"> <img src="images/proposition_stage.jpg"> </a></li>
-          <li><a href="#"> <img src="images/portefolio_etudiant.jpg"> </a></li>
-          <li><a href="#"> <img src="images/etudiants.jpg"> </a></li>
-          <li><a href="#"> <img src="images/portefolio1.jpg"> </a></li>
-        </ul>
-
-    
-      </nav>
-      <!-- /nav -->
-    </aside>
-    <!-- / content body -->
-  </div>
-</div>
+    </div><!--FIN content -->
   <!-- Footer -->
       <div class="clear">
       <footer >
