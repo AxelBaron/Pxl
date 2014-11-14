c6ff1c9778816ad45fe2fc41011948edef49c3cd
AxelBaron <axelbaron@outlook.com>
2014-11-14 13:45:52 -0500

    Correction form ajouter page
    …

--- admin/traitement-ajouter-page.php
+++ admin/traitement-ajouter-page.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Formulaire d'ajout de Page</title>
-		<meta charset="UTF-8"/>
-	</head>
-	
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style_admin.css">
+	<title>#TIM Matane - Administration principale</title>
+</head>
+<body>
 
-	<body>
+	<?php include("menu-admin.php");?>
+
+	<div class="right">
+
+		<section>
 	
 	<h1>Administration #TIM Matane</h1>	
 	<h2>Page Ajoutée</h2>
@@ -34,9 +39,13 @@
 
 		$sql = "INSERT INTO page(titre, resume,nav_page_nom_1, nav_page_lien_1,nav_page_nom_2,nav_page_lien_2,nav_page_nom_3, nav_page_lien_3,nav_page_nom_4, nav_page_lien_4,nav_page_nom_5, nav_page_lien_5,nav_page_nom_6, nav_page_lien_6) 
 			VALUES('$titre', '$resume','$nav_page_nom_1','$nav_page_lien_1','$nav_page_nom_2','$nav_page_lien_2','$nav_page_nom_3','$nav_page_lien_3','$nav_page_nom_4','$nav_page_lien_4','$nav_page_nom_5','$nav_page_lien_5','$nav_page_nom_6','$nav_page_lien_6')";
-		echo $sql;
-		//$pdo->exec($sql);
+		$pdo->exec($sql);
 	?>
+	</section>
+	</div>
+
+	</body>
+</html>
 	
 	</body>
 </html>
