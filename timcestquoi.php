fabb2164e69c08b09522de47c3f54393b3ecb86b
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-18 09:54:22 -0500

    Page timcestquoi terminée -sauf footer-

--- timcestquoi.php
+++ timcestquoi.php
@@ -19,11 +19,11 @@
         <article id="descriptionproweb">
             
               <h2>#TIM C'est quoi? </h2>
-              <li> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
               Integer non dapibus ligula. Morbi finibus tortor sit amet ex maximus, sed elementum sapien sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Vivamus dapibus finibus ante, vitae tempus velit aliquet sed. Morbi rhoncus lorem quis felis mattis, vitae tincidunt quam malesuada. 
               Aenean tincidunt turpis non arcu elementum varius. In quis tortor sit amet nulla consectetur malesuada in id nulla. 
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. </p></li>
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. </p>
               
               <h2 id="lienancre"><a href="#">Les matières de Multi <br />
               VS<br />
@@ -99,24 +99,38 @@
               Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
             </p>
             <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
+              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;" />
             </p>
           </div>
         </div>
       </div>
+
       <div id="conteneurVS">
         <h1>TIM VS les autre départements</h1>
         <div id="VS">
-        
-        </div>
+          <h2>Production Multimédia</h2>
+            <p class="txtalign">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
+              Integer non dapibus ligula. Morbi finibus tortor sit amet ex maximus, sed elementum sapien sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Vivamus dapibus finibus ante, vitae tempus velit aliquet sed. Morbi rhoncus lorem quis felis mattis, vitae tincidunt quam malesuada. 
+              Aenean tincidunt turpis non arcu elementum varius. In quis tortor sit amet nulla consectetur malesuada in id nulla. 
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. 
+            </p>
+            <p class="imgalign">
+              <img src="images/comparaison.jpg" alt="tableau comparatif" title="tableau comparatif" width="90%" height="90%" />
+            </p>
+        </div><!-- FIN VS -->
+      </div><!-- FIN CONTENEURVS -->
 
-      </div>
+      <div id="citations">
+        <p class="cita">"Une formation qui a de l'avenir"</p>
+      </div><!-- FIN CITATIONS -->
         
       
     </div><!--FIN content -->
   <!-- Footer -->
       <div class="clear">
-      <footer >
+      <footer>
       <p>Copyright &copy; 2014 - Tout droits réservé à Technique d'intégration multimédia Matane </p>
      </footer>
 </div>
