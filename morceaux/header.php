a1dd017da952588b7e898127c699ff76ba86ad9c
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-18 13:50:18 -0500

    Bon logo remis

--- morceaux/header.php
+++ morceaux/header.php
@@ -14,13 +14,48 @@
          <a href="index.php"><img src="images/TIMlogo2.svg" alt="Logo TIM" width="240" height="88"></a>
          </div>
       <ul>
-        <li><a href="#">Actualités</a></li>
-        <li><a href="#">Programme</a></li>
-        <li><a href="#">Réalisations </a></li>
-        <li><a href="#">Cégep</a></li>
-        <li><a href="#">Étudier à Matane</a></li>
-        <li><a href="#">Stages</a></li>
-        <li><a href="#">Contact</a></li>
+        <li>
+          <a href="#">Actualités</a>
+        </li>
+        <li>
+          <a href="#">Programme</a>
+          <ul>
+            <a href="">Tim c'est quoi?</a>
+            <a href="">Grille de cours</a>
+            <a href="">Après TIM?</a>
+            <a href="">L'équipe</a>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Réalisations </a>
+          <ul>
+            <a href="">3e année</a>
+            <a href="">1e et 2e année</a>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Cégep</a>
+          <ul>
+            <a href="">Locaux</a>
+            <a href="">Matériel</a>
+            <a href="">Club</a>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Étudier à Matane</a>
+          <ul>
+            <a href="">Passerelle DEP</a>
+            <a href="">International</a>
+            <a href="">CDRIN</a>
+            <a href="">Région de Matane</a>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Stages</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
       </ul>
     </nav>
   </header>
