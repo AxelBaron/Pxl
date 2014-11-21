27d85cbe1adec0937c7021883467a448ed14b580
AxelBaron <axelbaron@outlook.com>
2014-11-21 13:04:21 -0500

    Re comit, dsl kévin...
    …

--- admin/connectionbdd.php
+++
@@ -1,8 +0,0 @@
-    <?php
-        $hote ="localhost";
-        $usager ="root";
-        $mdp = "root";
-        $base = "tim_pefceq314";
-        $dns = "mysql:dbname=".$base.';host='.$hote;
-        $pdo= new PDO($dns, $usager, $mdp); 
-    ?>
