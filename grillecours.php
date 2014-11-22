5e2ddfb0fb57eae75bcff098eca84af239fc74b1
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-22 15:25:57 -0500

    Réalisation page "grille de cours" en cours

--- grillecours.php
+++ grillecours.php
@@ -11,120 +11,393 @@
      <!-- #tim? Image -->
 
       <section id="services" class="clear"> 
-          <img src="images/timhr.png" alt="tim?" title="tim?" width="960" height="104">
+          <img src="images/cours.png" alt="cours" title="cours" width="960" height="104">
       </section>
       
    <!-- #TIM? image -->
-      <section>
-		<article id="proweb">
-				<img src="images/prosduweb.jpg" alt="Les pros du web" title="Les pros du web" width="412" height="400">
-		</article>
-        <article id="descriptionproweb">
-            
-              <h2>#TIM C'est quoi? </h2>
-              <li><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
-              Integer non dapibus ligula. Morbi finibus tortor sit amet ex maximus, sed elementum sapien sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Vivamus dapibus finibus ante, vitae tempus velit aliquet sed. Morbi rhoncus lorem quis felis mattis, vitae tincidunt quam malesuada. 
-              Aenean tincidunt turpis non arcu elementum varius. In quis tortor sit amet nulla consectetur malesuada in id nulla. 
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. </p></li>
-              
-              <h2 id="lienancre"><a href="#">Les matières de Multi <br />
-              VS<br />
-              Les autres départements</a></h2>
-        </article>
-      </section>
-
+      
     </div>
     <!-- main content -->
     <div id="content">
-      <section>
-        <?php include("morceaux/cinqpts.php") ?>
-      </section>
+      
 
-      <div id="conteneurmatieres">
-      <h1>Les matières</h1>
-        <div id="matieres">
+      <div id="content">
+      <h1>La grille des cours</h1>
+        	<table>
+        		<tr class="session">
+        			<td>1ère session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation Générale</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Écriture et littérature</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>3</td>
+        		</tr>
+        		<tr>
+        			<td>Anglais I</td>
+        			<td>2</td>
+        			<td>1</td>
+        			<td>3</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Santé et éducation physique</td>
+        			<td>1</td>
+        			<td>1</td>
+        			<td>1</td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spécifique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Domaine du multimédia</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>1</td>
+        		</tr>
+        		<tr>
+        			<td>Environnement informatique</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>1</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Illustration</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Conception de sites Web</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Algorithmique et interactivité</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="session">
+        			<td>2e session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation générale</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		<tr class="ligneun">
+        			<td>Littérature et imaginaire</td>
+        			<td>3</td>
+        			<td>1</td>
+        			<td>3</td>
+        		</tr>
+        		<tr>
+        			<td>L'être humain</td>
+        			<td>3</td>
+        			<td>0</td>
+        			<td>3</td>
+        		</tr>
+
+        		<tr class="ligneun">
+        			<td>Anglais II</td>
+        			<td>2</td>
+        			<td>1</td>
+        			<td>3</td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spécifique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Programmation de l'interactivité</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Ergonomie Web</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Graphisme et mise en page</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Architecture de l'information</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Animation</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="session">
+        			<td>3e session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation générale</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Littérature québecoise</td>
+        			<td>3</td>
+        			<td>1</td>
+        			<td>4</td>
+        		</tr>
+        		<tr>
+        			<td>L'éthique</td>
+        			<td>2</td>
+        			<td>1</td>
+        			<td>3</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Activité physique</td>
+        			<td>0</td>
+        			<td>2</td>
+        			<td>1</td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spéficique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Gestion de base de données</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Composition visuelle</td>
+        			<td>2</td>
+        			<td>1</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Scénarisation</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Conception sonore</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Tournage vidéo</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>3</td>
+        		</tr>
+        		<tr>
+        			<td>Montage Vidéo</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="session">
+        			<td>4e session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation générale</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Communication ou</td>
+        			<td>3</td>
+        			<td>1</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Expression et littérature</td>
+        			<td>3</td>
+        			<td>1</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Complémentaire 1</td>
+        			<td>3</td>
+        			<td>0</td>
+        			<td>3</td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spécifique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Langage serveur Web</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Communication et médias</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>1</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Design graphique</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Conception graphique</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Monde professionnel</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Réalisation multimédia</td>
+        			<td>2</td>
+        			<td>4</td>
+        			<td>3</td>
+        		</tr>
+        		
+        		<tr class="session">
+        			<td>5e session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation générale</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Intégration de l'activité physique à son mode de vie</td>
+        			<td>1</td>
+        			<td>1</td>
+        			<td>1</td>
+        		</tr>
+        		<tr>
+        			<td>Complémentaire 2</td>
+        			<td>3</td>
+        			<td>0</td>
+        			<td>3</td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spécifique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Gestion de projet</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>1</td>
+        		</tr>
+        		<tr>
+        			<td>Imagerie 3D</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Design d'interface</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr>
+        			<td>Application interactive</td>
+        			<td>2</td>
+        			<td>2</td>
+        			<td>2</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Production multimédia</td>
+        			<td>2</td>
+        			<td>8</td>
+        			<td>4</td>
+        		</tr>
+        		<tr class="session">
+        			<td>6e session</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="formation">
+        			<td>Formation spécifique</td>
+        			<td></td>
+        			<td></td>
+        			<td></td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Préparation à l'épreuve-synthèse</td>
+        			<td>1</td>
+        			<td>2</td>
+        			<td>3</td>
+        		</tr>
+        		<tr>
+        			<td>Projet de fin d'études</td>
+        			<td>3</td>
+        			<td>10</td>
+        			<td>5</td>
+        		</tr>
+        		<tr class="ligneun">
+        			<td>Stage</td>
+        			<td>0</td>
+        			<td>14</td>
+        			<td>1</td>
+        		</tr>
+        	</table>
           
-          <div id="mat1">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
-            </p>
-          </div>
-          <div id="mat2">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
-            </p>
-          </div>
-          <div id="mat3">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
-            </p>
-          </div>
-          <div id="mat4">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
-            </p>
-          </div>
-          <div id="mat3">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;">
-            </p>
-          </div>
-          <div id="mat4">
-            <h2>Matière 1</h2>
-            <p>
-              Lipsum psum de quelques lignes qui permettra de comprendre ce qu'est la matière de façon très brève.
-              Mais il faudra qu'elle fasse quelques lignes, minimum 5, car sinon ça fait vraiment trop court pour le carré. Alors je mets encore un peu de lipsum psam bam.
-            </p>
-            <p class="imgillu">
-              <img src="lapin" alt="la petite image qui illustre" title="image qui illustre" width="370" height="100px;" />
-            </p>
-          </div>
-        </div>
       </div>
 
-      <div id="conteneurVS">
-        <h1>TIM VS les autre départements</h1>
-        <div id="VS">
-          <h2>Production Multimédia</h2>
-            <p class="txtalign">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique elementum dolor, maximus vestibulum ante varius id. 
-              Integer non dapibus ligula. Morbi finibus tortor sit amet ex maximus, sed elementum sapien sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Vivamus dapibus finibus ante, vitae tempus velit aliquet sed. Morbi rhoncus lorem quis felis mattis, vitae tincidunt quam malesuada. 
-              Aenean tincidunt turpis non arcu elementum varius. In quis tortor sit amet nulla consectetur malesuada in id nulla. 
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed a urna vulputate eros tempor consectetur. 
-            </p>
-            <p class="imgalign">
-              <img src="images/comparaison.jpg" alt="tableau comparatif" title="tableau comparatif" width="90%" height="90%" />
-            </p>
-        </div><!-- FIN VS -->
-      </div><!-- FIN CONTENEURVS -->
+      
 
+	<div class="clear"></div>
       <div id="citations">
         <p class="cita">"Une formation qui a de l'avenir..."</p>
       </div><!-- FIN CITATIONS -->
