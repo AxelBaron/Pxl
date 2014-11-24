c5f7ab9a2bd4dd7e8b2e2b5dd87fc89ab932a746
AxelBaron <axelbaron@outlook.com>
2014-11-24 00:16:49 -0500

    Contenu et CSS page international
    poil

--- international.php
+++ international.php
@@ -13,10 +13,66 @@
       <section id="services" class="clear"> 
           <img src="images/cours.png" alt="cours" title="cours" width="960" height="104">
       </section>
+    </div>
       
    <!-- #TIM? image -->
+   <section>
+        <article id="proweb">
+            <img src="images/prosduweb.jpg" alt="Les pros du web" title="Les pros du web" width="412" height="400">
+        </article>
+        <article id="descriptionproweb">
+              
+                <h2>#Etudiants internationnaux </h2>
+                <li><p>Découvrez dans cette section tout ce qui est à savoir pour les étudiants internationnaux qui souhaitent étudier à Matane. </p></li>
+                
+                <h2 id="lienancre"><a href="#">Le Cegep accueil les etudiants</a></a></h2>
+                <h2 id="lienancre"><a href="#">Procédures reliées à l'admission des étudiants internationaux</a></a></h2>
+                
+        </article>
+      </section>
+      <div class="clear"></div>
+
+      <div class="container">
+        <section>
+         
+        <article>
+          <h1>Le Cegep accueil les etudiants</h1> 
+            <img src="" alt=""/>
+            <p>Dans le cadre des ses priorités institutionnelles, la mobilité étudiante occupe une place importante au Cégep de Matane. Par un séjour d’études, un stage ou un travail interinstitutionnel, les étudiants de divers programmes d’études peuvent concrétiser une expérience à l’étranger dans le cadre de leurs études.</p>
+
+            <p>Les étudiants en Techniques d’intégration multimédia peuvent poser leur candidature pour un séjour d’une session à l’IUT de Saint-Dié des Vosges en France. La session ou plutôt, le semestre du programme français Services et Réseaux de Communication, comprend dix semaines d’études et dix semaines de stage (avec plusieurs journées de congé incluses!). À chaque année, depuis l’hiver 2006, des étudiants du Cégep de Matane et de l’IUT de Saint-Dié des Vosges vivent une expérience inoubliable qui enrichit leur curriculum tant professionnel que personnel.</p>
+
+            <p>Un étudiant international trouvera plusieurs avantages à la poursuite de ses études au Cégep de Matane en Techniques d'intégration multimédia :
+            La possibilité d'obtenir une exemption de ses frais de scolarité grâce à un programme de l'organisme Cégep international</p>
+
+              <!-- En atente de d'images du clients -->
+              <div class="en-attente-gauche"></div>
+              <div class="en-attente-droite"></div>
+              <div class="en-attente-gauche"></div>
+              <div class="en-attente-droite"></div>
+        </article>
+      </section>
+
+      <section>
+         
+        <article>
+          <h1>Procédures reliées à l'admission des étudiants internationaux</h1> 
+            <img src="" alt=""/>
+            <p>Visitez le site du Cégep de Matane pour connaître les <a href="http://www.cegep-matane.qc.ca/programmes-admission/bienvenue-aux-etudiants-etrangers/procedures-admission">procédures d'admission des étudiants étrangers.</a></p>
+
+            <p>Si vous avez d'autres questions, n'hésitez surtout pas à nous contacter par courriel à l'adresse <a href="mailto:information@cegep-matane.qc.ca">information@cegep-matane.qc.ca</a> ou par téléphone en composant le 418 562-1240 poste 2154.</p>
+
+
+              <!-- En atente de d'images du clients -->
+              <div class="en-attente-gauche"></div>
+              <div class="en-attente-droite"></div>
+              <div class="en-attente-gauche"></div>
+              <div class="en-attente-droite"></div>
+        </article>
+      </section>
+      </div>
       
-    </div>
+    
     <!-- main content -->
     <div id="content">
     	<div id="content"></div>
