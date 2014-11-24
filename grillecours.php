6a1847ec6df5aa296338b7acdda6d43420d8afc5
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-23 21:41:57 -0500

    Page "Grille de cours" Faite

--- grillecours.php
+++ grillecours.php
@@ -23,375 +23,298 @@
 
       <div id="content">
       <h1>La grille des cours</h1>
-        	<table>
-        		<tr class="session">
-        			<td>1ère session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation Générale</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Écriture et littérature</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>3</td>
-        		</tr>
-        		<tr>
-        			<td>Anglais I</td>
-        			<td>2</td>
-        			<td>1</td>
-        			<td>3</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Santé et éducation physique</td>
-        			<td>1</td>
-        			<td>1</td>
-        			<td>1</td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spécifique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Domaine du multimédia</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>1</td>
-        		</tr>
-        		<tr>
-        			<td>Environnement informatique</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>1</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Illustration</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Conception de sites Web</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Algorithmique et interactivité</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="session">
-        			<td>2e session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation générale</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		<tr class="ligneun">
-        			<td>Littérature et imaginaire</td>
-        			<td>3</td>
-        			<td>1</td>
-        			<td>3</td>
-        		</tr>
-        		<tr>
-        			<td>L'être humain</td>
-        			<td>3</td>
-        			<td>0</td>
-        			<td>3</td>
-        		</tr>
 
-        		<tr class="ligneun">
-        			<td>Anglais II</td>
-        			<td>2</td>
-        			<td>1</td>
-        			<td>3</td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spécifique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Programmation de l'interactivité</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Ergonomie Web</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Graphisme et mise en page</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Architecture de l'information</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Animation</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="session">
-        			<td>3e session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation générale</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Littérature québecoise</td>
-        			<td>3</td>
-        			<td>1</td>
-        			<td>4</td>
-        		</tr>
-        		<tr>
-        			<td>L'éthique</td>
-        			<td>2</td>
-        			<td>1</td>
-        			<td>3</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Activité physique</td>
-        			<td>0</td>
-        			<td>2</td>
-        			<td>1</td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spéficique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Gestion de base de données</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Composition visuelle</td>
-        			<td>2</td>
-        			<td>1</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Scénarisation</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Conception sonore</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Tournage vidéo</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>3</td>
-        		</tr>
-        		<tr>
-        			<td>Montage Vidéo</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="session">
-        			<td>4e session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation générale</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Communication ou</td>
-        			<td>3</td>
-        			<td>1</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Expression et littérature</td>
-        			<td>3</td>
-        			<td>1</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Complémentaire 1</td>
-        			<td>3</td>
-        			<td>0</td>
-        			<td>3</td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spécifique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Langage serveur Web</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Communication et médias</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>1</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Design graphique</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Conception graphique</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Monde professionnel</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Réalisation multimédia</td>
-        			<td>2</td>
-        			<td>4</td>
-        			<td>3</td>
-        		</tr>
-        		
-        		<tr class="session">
-        			<td>5e session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation générale</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Intégration de l'activité physique à son mode de vie</td>
-        			<td>1</td>
-        			<td>1</td>
-        			<td>1</td>
-        		</tr>
-        		<tr>
-        			<td>Complémentaire 2</td>
-        			<td>3</td>
-        			<td>0</td>
-        			<td>3</td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spécifique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Gestion de projet</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>1</td>
-        		</tr>
-        		<tr>
-        			<td>Imagerie 3D</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Design d'interface</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr>
-        			<td>Application interactive</td>
-        			<td>2</td>
-        			<td>2</td>
-        			<td>2</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Production multimédia</td>
-        			<td>2</td>
-        			<td>8</td>
-        			<td>4</td>
-        		</tr>
-        		<tr class="session">
-        			<td>6e session</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="formation">
-        			<td>Formation spécifique</td>
-        			<td></td>
-        			<td></td>
-        			<td></td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Préparation à l'épreuve-synthèse</td>
-        			<td>1</td>
-        			<td>2</td>
-        			<td>3</td>
-        		</tr>
-        		<tr>
-        			<td>Projet de fin d'études</td>
-        			<td>3</td>
-        			<td>10</td>
-        			<td>5</td>
-        		</tr>
-        		<tr class="ligneun">
-        			<td>Stage</td>
-        			<td>0</td>
-        			<td>14</td>
-        			<td>1</td>
-        		</tr>
-        	</table>
+      		<table>
+      		<tr class="annee"><td colspan="8">1e année</td></tr>
+      		<tr class="session">
+      			<td colspan="4">1ère session</td>
+
+      			<td colspan="4">2ème session</td>
+      		</tr>
+      		<tr class="formation">
+      			<td colspan="4">Formation Générale</td>
+      			<td colspan="4">Formation Générale</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td >Écriture et littérature</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">3</td>
+      			<td>Littérature et imaginaire</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		<tr>
+      			<td>Anglais I</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffreslast">3</td>
+      			<td>L'être humain</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">0</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffreslast"></td>
+      			<td>Anglais II</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		<tr class="formation">
+      			<td colspan="4">Formation spécifique</td>
+      			<td colspan="4">Formation spécifique</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Domaine du multimédia</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">1</td>
+      			<td>Programmation de l'interactivité</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>Environnement informatique</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">1</td>
+      			<td>Ergonomie Web</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Illustration</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Graphisme et mise en page</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>Conception de sites Web</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Architecture de l'information</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Algorithmique et interactivité</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Animation</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		</table>
+      		<!-- DEUXIÈME TABLE -->
+      		<table>
+      		<tr class="annee"><td colspan="8">2e année</td></tr>
+      		<tr class="session">
+      			<td colspan="4">3e session</td>
+
+      			<td colspan="4">4e session</td>
+      		</tr>
+      		<tr class="formation">
+      			<td colspan="4">Formation Générale</td>
+      			<td colspan="4">Formation Générale</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td >Littérature Québecoise</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffreslast">4</td>
+      			<td>Communication ou</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>L'éthique</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffreslast">3</td>
+      			<td>Expression et littérature</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Activité physique</td>
+      			<td class="chiffres">0</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">1</td>
+      			<td>Complémentaire 1</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">0</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		<tr class="formation">
+      			<td colspan="4">Formation spécifique</td>
+      			<td colspan="4">Formation spécifique</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Gestion de base de données</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Langage serveur Web</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>Composition visuelle</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Communication et médias</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">1</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Scénarisation</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Design graphique</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>Conception sonore</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Conception graphique</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Tournage vidéo</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">3</td>
+      			<td>Monde professionnel</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      		</tr>
+      		<tr>
+      			<td>Montage vidéo</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Réalisation multimédia</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">4</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		</table>
+      		<!-- TROISIÈME TABLE -->
+      		<table>
+      		<tr class="annee"><td colspan="8">3e année</td></tr>
+      		<tr class="session">
+      			<td colspan="4">4e session</td>
+
+      			<td colspan="4">5e session</td>
+      		</tr>
+      		<tr class="formation">
+      			<td colspan="4">Formation Générale</td>
+      			<td colspan="4">Formation Générale</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td >Intégration de l'activité physique à son mode de vie</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffreslast">1</td>
+      			<td></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      		</tr>
+      		<tr>
+      			<td>Complémentaire 2</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">0</td>
+      			<td class="chiffreslast">3</td>
+      			<td></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      		</tr>
+      		
+      		<tr class="formation">
+      			<td colspan="4">Formation spécifique</td>
+      			<td colspan="4">Formation spécifique</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Gestion de projet</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">1</td>
+      			<td>Préparation à l'épreuve synthèse</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">3</td>
+      		</tr>
+      		<tr>
+      			<td>Imagerie 3D</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Projet de fin d'études</td>
+      			<td class="chiffres">3</td>
+      			<td class="chiffres">10</td>
+      			<td class="chiffres">5</td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Design d'interface</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td>Stage</td>
+      			<td class="chiffres">0</td>
+      			<td class="chiffres">14</td>
+      			<td class="chiffres">1</td>
+      		</tr>
+      		<tr>
+      			<td>Application interactive</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">2</td>
+      			<td></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      		</tr>
+      		<tr class="ligneun">
+      			<td>Production multimédia</td>
+      			<td class="chiffres">1</td>
+      			<td class="chiffres">2</td>
+      			<td class="chiffreslast">3</td>
+      			<td></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      			<td class="chiffres"></td>
+      		</tr>
+      		</table>
           
       </div>
 
