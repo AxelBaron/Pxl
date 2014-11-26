2626009172ff5eb4307b86e81bc28d376f5a146a
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-11-26 14:53:13 -0500

    Poil
    ..

--- cdrin.php
+++ cdrin.php
@@ -15,16 +15,17 @@
       </section>
       <section>
         <article id="proweb">
-            <img src="images/international.jpg" alt="Les pros du web" title="Les pros du web" width="312" height="300">
+            <img src="images/CDRIN.jpg" alt="CDRIN" title="CDRIN" width="312" height="300">
         </article>
         <article id="descriptionproweb">
               
                 <h2>CDRIN</h2>
                 <p>Le Centre de développement et de recherche en imagerie numérique, associé au Cégep de Matane, ce tout nouveau centre de recherche doté d’un studio de capture de mouvements unique au Québec, est situé au 608 avenue Saint-Rédempteur à Matane au Bas-Saint-Laurent.</p>
                 
-                <h2 id="lienancre"><a href="#dematane">De Matane à...</a></h2>
-                <h2 id="lienancre"><a href="#jusquematane">De ... jusque Matane</a></h2>
-                <h2 id="lienancre"><a href="#procédures">Procédures</a></h2>
+                <h2 id="lienancre"><a href="#presentation">Présentation</a></h2>
+                <h2 id="lienancre"><a href="#servicescdrin">Services</a></h2>
+                <h2 id="lienancre"><a href="#infrastructure">Infrastructure</a></h2>
+                <h2 id="lienancre"><a href="#axesderecherche">Axes de recherche</a></h2>
                 
         </article>
       </section>
@@ -36,123 +37,77 @@
     <!-- main content -->
     <div id="content">
       <div id="conteneurprincipal">
-        <h1 id="dematane">De Matane à partout dans le monde (ou presque!)</h1>
+        <h1>CDRIN</h1>
         <div class="conteneursecondaire">
-          <h2>Conditions d'admission</h2>
+          <h2 id="presentation">Présentation</h2>
+          <p>
+            Le Centre de développement et de recherche en imagerie numérique, le CDRIN, effectue de la recherche et du transfert technologique auprès de la petite, de la moyenne et de la grande entreprise dans le domaine de l’imagerie numérique, notamment en capture de mouvements et en réalité augmentée. Il répond également aux besoins de développement des compétences et de perfectionnement de la main-d’œuvre de ce secteur en constante évolution.
+          </p>
+
+          <h2 id="servicescdrin">Services</h2>
           <p>
             <ul>
-              <li>Être étudiant au DEC en Intégration multimédia</li>
-              <li>Avoir réussi les matières des sessions 1 à 5</li>
-              <li>Déposer sa candidature</li>
+              <li>Aide technique et technologique</li>
+              <li>Assistance pour l’obtention de fonds de recherche</li>
+              <li>Accès à des bancs d’essais technologiques</li>
+              <li>Support à l’expérimentation réalisée avec les infrastructures du CDRIN</li>
+              <li>Offre de formation liée à la recherche et au développement</li>
+              <li>Offres de stages et d’emplois dans le milieu</li>
             </ul>
           </p>
 
-          <h2>Description du projet</h2>
-          <p>
-          Depuis 2006, le Cégep de Matane offre aux étudiants du DEC en Intégration multimédia la possibilité de compléter leur sixième session dans un IUT partenaire en France. Le semestre comprend huit semaines d'études et dix semaines de stage s'échelonnant de la mi-janvier à la mi-juin et correspond au dernier semestre du programme de DUT SRC.
-          </p>
-          <p>
-          Le projet s’inscrit dans le cadre d’une entente interinstitutionnelle qui définit les activités du séjour assurant l'encadrement interinstitutionnelle ainsi que la reconnaissance scolaire.
-          </p>
-          <p>
-          L'étudiant recevra le relevé de notes officiel de l'IUT en plus de voir sa sixième session reconnue dans son DEC. La réussite scolaire est essentielle.
-          </p>
+          <h2 id="infrastructure">Infrastructure</h2> 
 
-          <p>
-          Les étudiants en Techniques d’intégration multimédia peuvent poser leur candidature pour un séjour d’une session à l’IUT de Saint-Dié des Vosges en France. 
-          La session ou plutôt, le semestre du programme français Services et Réseaux de Communication, comprend dix semaines d’études et dix semaines de stage (avec plusieurs journées de congé incluses!).
-          
-          Des étudiants du Cégep de Matane peuvent vivre ainsi une expérience inoubliable qui enrichit leur curriculum tant professionnel que personnel.
-          </p>
-
-          <h2>Les avantages spécifiques</h2> 
           <p>
             <ul>
               <li>
-                Un enrichissement professionnel dans le cadre de la mondialisation des marchés.
-              </li>
+                Studio de capture de mouvements de  30,5 mètres de longueur x 24 mètres de largeur x 10 mètres de hauteur</li>
+              <li>Structure autoportante de 28 mètres de longueur x 22 mètres de largeur x3 mètres de hauteur comportant 4 arches de 6 mètres qui traversent le studio en largeur</li>
+              <li>Pont des cascadeurs équipé d’un système de câbles permettant à deux comédiens cascadeurs d’effectuer de la voltige</li>
               <li>
-                Une expérience fort pertinente dans le contexte des importants besoins de main-d’oeuvre de l’industrie québécoise des technologies de l’information et du divertissement numérique.
+                Salle de présentation
               </li>
-              <li>
-                Une expérience de stage à l'européenne.
-              </li>
+              <li>Plus d’une dizaine de bureaux de chercheurs équipés d’ordinateurs performants</li>
+              <li>Suite locative de 258 m2 reconnue à titre de carrefour de la nouvelle économie pouvant être occupée par une entreprise</li>
             </ul>
+            <p class="imgalign">
+              <img src="images/cdrinstudio.jpg" alt="CDRIN studio" width="960" height="547">
+            </p>
           </p>
-          <h2>Partenaires</h2>
+
+          <h2 id="axesderecherche">Axes de recherche</h2>
+          <h3>Imagerie numérique (Axe développé avec l'UQAR)</h3>
           <p>
             <ul>
-              <li>IUT Saint-Dié des Vosges</li>
-              <li>IUT Montbéliard (en cours)</li>
-              <li>IUT Chambéry (en cours)</li>
+              <li>Vision numérique</li>
+              <li>Développement logiciel et matériel</li>
             </ul>
           </p>
-        
+          <h3>Numérisation et analyse du mouvement</h3>
+          <p>
+            <ul>
+              <li>Optimisation des systèmes de capture du mouvement</li>
+              <li>Hybridation des systèmes de capture (matériel et logiciel)</li>
+              <li>Développement de procédés innovateurs pour l'analyse et l'extraction de données numériques</li>
+              <li>Optimisation de l'efficacité du processus de capture du mouvement</li>
+            </ul>
+          </p>
+          <h3>Interface homme-machine, immersion et réalité virtuelle</h3>
+          <p>
+            <ul>
+              <li>Optimisation et expérimentation des étapes de production impliquant des interfaces</li>
+              <li>Expérimentation d'interface matérielle et logicielle</li>
+              <li>Application des technologies d'interface dans le domaine de la santé</li>
+              <li>Application des technologies d'interface dans les domaines de l'ingénierie et de l'ergonomie</li>
+              <li>Application des technologies d'interface dans le domaine des médias interactifs</li>
+            </ul>
+          </p>
         </div>
-
-        <h1 id="jusquematane">De partout dans le monde (ou presque!) à Matane</h1>
-        <div class="conteneursecondaire">
-        <h2>Conditions d'admission</h2>
-        <p>
-          <ul>
-            <li>Être étudiant au DUT en Services et réseaux de communications (SRC)</li>
-            <li>Avoir de bons résultats scolaires</li>
-            <li>Être sélectionné par son IUT et le Cégep de Matane</li>
-          </ul>
-        </p>
-
-        <h2>Description du projet</h2>
-        <p>
-        Le Cégep de Matane offre un projet d’échange en mobilité étudiante aux étudiants du programme d’études québécois DEC Intégration multimédia et du programme d’études français DUT en SRC.  Les deux programmes d’études mènent à un diplôme d’État BAC+2. Le projet s’inscrit dans le cadre d’une entente interinstitutionnelle définissant les activités du séjour, l’encadrement interinstitutionnel ainsi que la reconnaissance de la réussite scolaire.
-        </p>
-
-        <p>Un étudiant international trouvera des avantages à la poursuite de ses études au Cégep de Matane. Une fois son admission effectuée, le cégep peut soumettre sa candidature dans le cadre du Programme de bourses et d’exemptions de frais de scolarité du ministère de l’Éducation du Québec géré par Cégep International. Pour plus d’information, consulter le site Web de Cégep International. </p>
-
-        <h2>Avantages</h2>
-        <p>Un étudiant international trouvera plusieurs avantages à la poursuite de ses études au Cégep de Matane en Techniques d'intégration multimédia :
-
-        <ul>
-          <li>Un séjour scolaire dans un secteur d’activité en pleine effervescence au Québec!</li>
-          <li>La formation en Multimédia suit les tendances Web et les développements pour appareils mobiles. Au programme : de la gestion de projet, de l’imagerie 3D, du développement Web, du développement flash et de la création pour les médias sociaux!</li>
-          <li>Un accès aux salles d'ordinateurs avec Internet, les soirs et les week-ends!</li>
-          <li>Des étudiants de quelques autres nationalités inscrits dans divers programmes d’études contribuent à un environnement multiculturel chaleureux!</li>
-          <li>Une expérience à la nord-américaine!</li>
-          <li>Un campus avec résidences, plateaux sportifs et clubs parascolaires!</li>
-          <li>Exemption des frais de scolarité pour les étudiants possédant la nationalité française (Accords France-Québec)!</li>
-          <li>Assurance médicale pour les étudiants possédant la nationalité française (Accords France-Québec)!</li>
-        </ul>
-        </p>
-
-        <h2>Pour vous inscrire</h2>
-        <p>
-        Demander à votre école de communiquer avec la responsable de la mobilité internationale au Cégep de Matane.
-        </p>
-
-        <p class="imgalign"><img src="http://www.travelhouse.ch/CMS/18/3023930/2/matane-matane.jpg" alt="Matane" title="Matane"></p>
-        </p>
-     
-        </div>
-
-        <h1 id="procédures">Procédures reliées à l'admission des étudiants internationaux</h1>
-        <div class="conteneursecondaire">
-        <p>Visitez la page "<a href="http://www.cegep-matane.qc.ca/activites-internationales/bienvenue-aux-etudiants-internationaux">bienvenue aux étudiants internationaux</a>" du Cégep de Matane pour connaître les procédures d'admission des étudiants étrangers. </p>
-
-        <p>Si vous avez d'autres questions, n'hésitez surtout pas à nous contacter par :
-        <ul>
-          <li>Site du Cégep : <a href="http://www.cegep-matane.qc.ca/">http://www.cegep-matane.qc.ca/</a></li>
-          <li>Courriel : <a href="mailto:information@cegep-matane.qc.ca">information@cegep-matane.qc.ca</a></li>
-          <li>Téléphone : 418 562-1240 poste 2154</li>
-        </ul>
-        </p>
-      </div>
-
 
       <div class="clear"></div>
       <div id="citations">
         <p class="cita">"Une formation qui a de l'avenir..."</p>
-      </div><!-- FIN CITATIONS -->
-        
-      
+      </div><!-- FIN CITATIONS --> 
     </div><!--FIN content -->
   
     <div class="clear">
--- regiondematane.php
+++ regiondematane.php
@@ -11,24 +11,91 @@
      <!-- #tim? Image -->
 
       <section id="services" class="clear"> 
-          <img src="images/cours.png" alt="cours" title="cours" width="960" height="104">
+          <img src="images/matane.png" alt="Matanie" title="Matanie" width="960" height="104">
       </section>
+      <section>
+        <article id="proweb">
+            <img src="images/matane4.jpg" alt="Matanie" title="Matanie" width="312" height="300">
+        </article>
+        <article id="descriptionproweb">
+              
+                <h2>La Matanie</h2>
+                <p>La Matanie est une municipalité régionale de comté (MRC) de la région administrative du Bas-Saint-Laurent, dans la province de Québec, au Canada. Créée le 1er janvier 1982 sous le nom de Matane, elle a été renommée La Matanie le 9 mars 20133,4. Elle est composée de douze municipalités et d'un territoire non organisé. Son chef-lieu et unique ville est Matane qui comprend 66 % de la population de la MRC. Son préfet actuel est Pierre Thibodeau5.</p>
+                
+                <h2 id="lienancre"><a href="#loisirs">Loisirs</a></h2>
+                <h2 id="lienancre"><a href="#tourisme">Aspects touristiques 2</a></h2>
+                
+                
+        </article>
+      </section>
+
       
    <!-- #TIM? image -->
       
     </div>
     <!-- main content -->
     <div id="content">
-    	<div id="content"></div>
-		<div class="clear"></div>
-		<div id="citations">
-        	<p class="cita">"Une formation qui a de l'avenir..."</p>
-      	</div><!-- FIN CITATIONS -->
+      <div id="conteneurprincipal">
+        <h1>Les avantages de la région</h1>
+        <div class="conteneursecondaire">
+          <h2 id="loisirs">Loisirs</h2>
+          <p>
+            Matane et sa région immédiate comptent des attraits et des loisirs recherchés et enviables :
+            <ul>
+              <li>Les salles de spectacle Lucien-Bellemare, Albert-B.-Lavoie et Le Barachois</li>
+              <li>Le cinéma Gaieté et le cinéma de répertoire offert par CinémAlice</li>
+              <li>Le Complexe culturel Joseph-Rouleau abritant la Galerie d'art de Matane, la galerie Espace F, la collection du peintre Claude Picher La Couleur de la Gaspésie, la salle Isabelle-Boulay/Smurfit-Stone et la bibliothèque municipale</li>
+              <li>Une fête de la voile : Cap sur Matane, les Fêtes du Québec maritime et toutes ces soirées où les spectacles organisés par le diffuseur Kaméléart deviennent des prétextes à la fête.</li>
+            </ul>
+          </p>
+
+          <h2 id="tourisme">Aspects touristiques</h2>
+          <p>
+            Des attraits touristiques locaux reconnus :
+            <ul>
+              <li>Un bord de mer propice à la ballade, au kayak de mer et à la voile, avec en prime des couchers de soleil inoubliables;</li>
+              <li>L'une des plus belles rivières à saumon d'Amérique : la rivière Matane;</li>
+              <li>La Promenade des capitaines et le parc des Îles pour ses activités familiales ou sa piste de patins à roues alignées;</li>
+              <li>La réserve faunique de Matane recherchée par les amateurs de pêche à la truite et de chasse à l'orignal ou au cerf de Virginie;</li>
+              <li>Des sentiers aménagés pour le vélo de montagne ou la randonnée pédestre comme le Sentier international des Appalaches;</li>
+              <li>Des montagnes, les Chic-Chocs, et leurs paysages grandioses;</li>
+              <li>Des falaises de glace à gravir;</li>
+              <li>Le parc éolien Le Nordais, unique, splendide et à voir absolument à Saint-Ulric et Cap-Chat;</li>
+              <li>Et à proximité des attraits touristiques régionaux majeurs, tel le parc de la Gaspésie et les Jardins de Métis.</li>
+            </ul>
+            <p class="imgalign">
+              <img src="" alt="img quelconque" width="960" height="547">
+            </p>
+
+            Matane met aussi au service de sa population : une marina et un club de voile, une piscine, un aréna, un centre de ski, une piste de ski de fond, des courts de tennis, une piste de patins à roues alignées, un terrain de golf, des salons de quilles, des sentiers de motoneige, un centre d'activité physique, etc.
+
+
+          </p>
+        </div>
+
+      <div class="clear"></div>
+      <div id="citations">
+        <p class="cita">"Une formation qui a de l'avenir..."</p>
+      </div><!-- FIN CITATIONS --> 
     </div><!--FIN content -->
   
     <div class="clear">
 
     <?php include("morceaux/footer.php") ?>
 </div>
+
+<!-- appel du script qui servira au hover -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>  
+    
+    <script type="text/javascript">
+      $(function() {
+      
+        $(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
+
+      });
+</script>
+<script src="js/modernizr.custom.97074.js"></script>
+
 </body>
 </html>
