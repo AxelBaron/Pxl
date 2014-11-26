bf3dfaa7d0d2d07fa7b92a1e3f2db29a2a1efb28
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-11-26 17:17:16 -0500

    Update traitement-apparition-menu.php

--- admin/traitement-apparition-menu.php
+++ admin/traitement-apparition-menu.php
@@ -4,43 +4,35 @@
 
 <?php 
 	
-	// for ($i=1; $i < 8 ; $i++) { 
-	// 	$positionEtIDmenu = explode("_",$_POST["position$i"]);
-	// }
-
-
-	
-	print_r($_POST);
-	$position1 = $_POST['position1'];
-	$position2 = $_POST['position2'];
-	$position3 = $_POST['position3'];
-	$position4 = $_POST['position4'];
-	$position5 = $_POST['position5'];
-	$position6 = $_POST['position6'];
-	$position7 = $_POST['position7'];
-
-
-
-	if ($position1 == $position2 || $position1 == $position3 || $position1 == $position4 || $position1 == $position5 || $position1 == $position6 || $position1 == $position7) {
+	 for ($i=1; $i < 8 ; $i++) { 
+		$varContainer = "position".$i."EtIDmenu";
+		$$varContainer = explode("_",$_POST["position$i"]);
+	 }
+	 
+	 
+	if ($position1EtIDmenu[0] == $position2EtIDmenu[0] || $position1EtIDmenu[0] == $position3EtIDmenu[0] || $position1EtIDmenu[0] == $position4EtIDmenu[0] || $position1EtIDmenu[0] == $position5EtIDmenu[0] || $position1EtIDmenu[0] == $position6EtIDmenu[0] || $position1EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
-	}elseif($position2 == $position1 || $position2 == $position3 || $position2 == $position4 || $position2 == $position5 || $position2 == $position6 || $position2 == $position7) {
+	}elseif($position2EtIDmenu[0] == $position1EtIDmenu[0] || $position2EtIDmenu[0] == $position3EtIDmenu[0] || $position2EtIDmenu[0] == $position4EtIDmenu[0] || $position2EtIDmenu[0] == $position5EtIDmenu[0] || $position2EtIDmenu[0] == $position6EtIDmenu[0] || $position2EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";	
-	}elseif ($position3 == $position1 || $position3 == $position2 || $position3 == $position4 || $position3 == $position5 || $position3 == $position6 || $position3 == $position7) {
+	}elseif ($position3EtIDmenu[0] == $position1EtIDmenu[0] || $position3EtIDmenu[0] == $position2EtIDmenu[0] || $position3EtIDmenu[0] == $position4EtIDmenu[0] || $position3EtIDmenu[0] == $position5EtIDmenu[0] || $position3EtIDmenu[0] == $position6EtIDmenu[0] || $position3EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
-	}elseif ($position4 == $position1 || $position4 == $position2 || $position4 == $position3 || $position4 == $position5 || $position4 == $position6 || $position4 == $position7) {
+	}elseif ($position4EtIDmenu[0] == $position1EtIDmenu[0] || $position4EtIDmenu[0] == $position2EtIDmenu[0] || $position4EtIDmenu[0] == $position3EtIDmenu[0] || $position4EtIDmenu[0] == $position5EtIDmenu[0] || $position4EtIDmenu[0] == $position6EtIDmenu[0] || $position4EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
-	}elseif ($position5 == $position1 || $position5 == $position2 || $position5 == $position3 || $position5 == $position4 || $position5 == $position6 || $position5 == $position7) {
+	}elseif ($position5EtIDmenu[0] == $position1EtIDmenu[0] || $position5EtIDmenu[0] == $position2EtIDmenu[0] || $position5EtIDmenu[0] == $position3EtIDmenu[0] || $position5EtIDmenu[0] == $position4EtIDmenu[0] || $position5EtIDmenu[0] == $position6EtIDmenu[0] || $position5EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
-	}elseif ($position6 == $position1 || $position6 == $position2 || $position6 == $position3 || $position6 == $position4 || $position6 == $position5 || $position6 == $position7) {
+	}elseif ($position6EtIDmenu[0] == $position1EtIDmenu[0] || $position6EtIDmenu[0] == $position2EtIDmenu[0] || $position6EtIDmenu[0] == $position3EtIDmenu[0] || $position6EtIDmenu[0] == $position4EtIDmenu[0] || $position6EtIDmenu[0] == $position5EtIDmenu[0] || $position6EtIDmenu[0] == $position7EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
-	}elseif ($position7 == $position1 || $position7 == $position2 || $position7 == $position3 || $position7== $position4 || $position7 == $position5 || $position7 == $position6) {
+	}elseif ($position7EtIDmenu[0] == $position1EtIDmenu[0] || $position7EtIDmenu[0] == $position2EtIDmenu[0] || $position7EtIDmenu[0] == $position3EtIDmenu[0] || $position7EtIDmenu[0]== $position4EtIDmenu[0] || $position7EtIDmenu[0] == $position5EtIDmenu[0] || $position7EtIDmenu[0] == $position6EtIDmenu[0]) {
 		echo "<p> Désolé, la modification n'as pas été prise en compte car plusieurs menus avait le même indice!";
 	}else{
 		echo "Tout est ok";
-		// for ($i=1; $i < 8; $i++) { 
-		//  	$sql ="UPDATE menu SET position = position".$i."WHERE menu_id =".$positionEtIDmenu[1];
-		//  	echo $sql;
+		 for ($i=1; $i < 8; $i++) { 
+			$varContainer = "position".$i."EtIDmenu";
+			$position = array_shift($$varContainer);
+			$id = array_shift($$varContainer);
+		  	$sql ="UPDATE menu SET position = $position WHERE menu_id = $id";
+			$pdo->exec($sql);
+		  	echo "<div>".$sql."</div>";
 		}
-		
 	}
  ?>
