b7208ef753658799cf16adaaf4cbf51042c33661
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-02 14:51:39 -0500

    WYSIWYG Form Ajout Contenu

--- admin/form-ajout-contenu.php
+++ admin/form-ajout-contenu.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="style_admin.css">
-	<title>#TIM Matane - Administration principale</title>
-</head>
-<body>
-
-	<?php include("menu-admin.php");?>
-
-	<div class="right">
-
-	<?php 
-		include("connectionbdd.php");
-	?>
-
-		<section>
-	
+	<?php include_once "header-admin.php"; ?>
 	<h1>Administration #TIM Matane</h1>	
 	<h2>Ajouter un Contenu</h2>
 
@@ -31,23 +13,44 @@
 			</div>
 			
 			<div>
-				<label for="contenu">Contenu</label>
-				<textarea class="form" name="contenu"></textarea>
+				<!--WYSIWYG-->
+				<textarea id="tinyeditor" style="width: 400px; height: 200px"></textarea>
+				<script>
+				var editor = new TINY.editor.edit('editor', {
+					id: 'tinyeditor',
+					width: 600,
+					height: 175,
+					cssclass: 'tinyeditor',
+					controlclass: 'tinyeditor-control',
+					rowclass: 'tinyeditor-header',
+					dividerclass: 'tinyeditor-divider',
+					controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
+						'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
+						'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
+						'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|', 'print'],
+					footer: true,
+					fonts: ['Verdana','Arial','Georgia','Trebuchet MS'],
+					xhtml: true,
+					bodyid: 'editor',
+					footerclass: 'tinyeditor-footer',
+					toggle: {text: 'source', activetext: 'wysiwyg', cssclass: 'toggle'},
+					resize: {cssclass: 'resize'}
+				});
+				</script>
+				<!--FIN WYSIWYG-->
+				<input type="hidden" id="contenu" name="contenu" value="" />
 			</div>
 
 			<div>
 				<label for="id_page_">A quelle page voulez vous affecter ce contenu ?</label>
 				<select name="id_page_">
 					<?php  
-						
 							$sql="SELECT * FROM page";
 							$req = $pdo->query($sql);
 		
 							while ( $data = $req->fetch()) {
 								echo "<option value=".$data['page_id'].">".$data["titre"]."</option>";
 							 }
-						
-					
 					?>
 				</select>
 			</div>
@@ -55,7 +58,7 @@
 		</fieldset>
 		
 	
-		<input type="submit" name="enregistrer" id="enregistrer" value="Enregistrer"/>
+		<input type="submit" name="enregistrer" id="enregistrer" onclick="document.getElementById('contenu').value = editor.e.body.innerHTML" value="Enregistrer"/>
 	</form>
 	</section>
 	</div>
