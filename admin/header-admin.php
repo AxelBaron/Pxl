0aa37244505bd5c304cd0bdcb131760b97d415e2
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-02 14:59:00 -0500

    Update pour le WYSIWYG

--- admin/header-admin.php
+++ admin/header-admin.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="style_admin.css">
+	<link rel="stylesheet" href="tinyeditor.css">
+	<script src="tiny.editor.js"></script>
 	<script type="text/javascript" src="jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="script.js"></script>
 	<title>#TIM Matane - Administration Actualités</title>
