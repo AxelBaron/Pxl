ab600351273af5c583f756a25273bfb0e6778d43
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-03 21:09:02 -0500

    Fonctionne avec les apostrophe
    et les balises HTML qui pourrait être insérer

--- admin/traitement-ajouter-contenu.php
+++ admin/traitement-ajouter-contenu.php
@@ -7,13 +7,26 @@
 
 		include('connectionbdd.php');
 
+		
+		$liste_de_filtres = array(
+		'titre' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'contenu' => FILTER_UNSAFE_RAW,
+		'id_page_' => FILTER_SANITIZE_STRING ,	
+		);
+		
+		$data_filtre = filter_input_array(INPUT_POST,$liste_de_filtres);
+		
 		$titre = $_POST['titre'];
 		$contenu = $_POST['contenu'];
 		$idpage = $_POST['id_page_'];
 
 
-		$sql = "INSERT INTO contenu(titre,contenu,id_page_) VALUES('$titre','$contenu','$idpage')";
-		$pdo->exec($sql);
+		$sql = "INSERT INTO contenu(titre,contenu,id_page_) VALUES(:titre,:contenu,:id_page_)";
+		$requete = $pdo->prepare($sql);
+		$requete->bindParam(':titre', $data_filtre['titre'], PDO::PARAM_STR);
+		$requete->bindParam(':contenu', $data_filtre['contenu'], PDO::PARAM_STR);
+		$requete->bindParam(':id_page_', $data_filtre['id_page_'], PDO::PARAM_STR);
+		$requete->execute();
 	?>
 	<a href="gestion-contenu.php"><button>Retour</button></a>
 <?php include("footer-admin.php"); ?>
