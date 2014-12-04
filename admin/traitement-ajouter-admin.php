52859db57af49b5abd7a597f93f4dc35b8d2658b
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-03 20:46:14 -0500

    Gere les apostrophes

--- admin/traitement-ajouter-admin.php
+++ admin/traitement-ajouter-admin.php
@@ -7,14 +7,32 @@
 
 		include('connectionbdd.php');
 
+		$liste_de_filtres = array(
+		'usager' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'mdp' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'nom' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'prenom' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,	
+		'courriel' => FILTER_SANITIZE_EMAIL,	
+		);
+		
+		$data_filtre = filter_input_array(INPUT_POST,$liste_de_filtres);
+		
 		$usager = $_POST['usager'];
 		$mdp = $_POST['mdp'];
 		$nom = $_POST['nom'];
 		$prenom = $_POST['prenom'];
 		$courriel = $_POST['courriel'];
 
-		$sql = "INSERT INTO admin(usager,mdp,nom,prenom,courriel) VALUES('$usager','$mdp','$nom','$prenom','$courriel')";
-		$pdo->exec($sql);
+		$sql = "INSERT INTO admin(usager,mdp,nom,prenom,courriel) VALUES(:usager,:mdp,:nom,:prenom,:courriel)";
+		$requete = $pdo->prepare($sql);
+		$requete->bindParam(':usager', $data_filtre['usager'], PDO::PARAM_STR);
+		$requete->bindParam(':mdp', $data_filtre['mdp'], PDO::PARAM_STR);
+		$requete->bindParam(':nom', $data_filtre['nom'], PDO::PARAM_STR);
+		$requete->bindParam(':prenom', $data_filtre['prenom'] ,PDO::PARAM_STR);
+		$requete->bindParam(':courriel', $data_filtre['courriel'], PDO::PARAM_STR);
+		$requete->execute();
+		
+		
 	?>
 	<a href="gestion-admin.php"><button>Retour</button></a>
 <?php include("footer-admin.php"); ?>
