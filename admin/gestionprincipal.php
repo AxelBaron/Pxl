3dba7d28fe1d7434620035dfbe51d6a538eb89ba
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-05 15:40:53 -0500

    Update gestionprincipal.php

--- admin/gestionprincipal.php
+++ admin/gestionprincipal.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="style_admin.css">
-	<title>#TIM Matane - Administration principale</title>
-</head>
-<body>
-
-	<?php include("menu-admin.php");?>
-
-	<div class="right">
-
-		<section>
+<?php
+include "header-admin.php";
+?>
 			<div id="fond_logo">	
 				<img src="../images/TIMlogo2.svg" alt="Logo tim Matane" id="logo">
 			</div>
