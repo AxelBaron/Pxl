e07722ccde996e173408c47aa9b64044069aa31e
AxelBaron <axelbaron@outlook.com>
2014-12-05 17:05:54 -0500

    Page equipe dynamic
    ….NAMIQUE

---
+++ equipe-dynamic.php
@@ -0,0 +1,56 @@
+<?php include("morceaux/header.php");?>
+
+<!-- content -->
+<div class="wrapper row2">
+  <div id="container">
+    <!-- Slider -->
+    <section id="slider"><a href="index.php"><img src="images/slider.jpg" alt="Slider1" width="960" height="230" /></a></section>
+    <!-- main content -->
+    <div id="homepage">
+
+     <!-- #tim? Image -->
+
+      <section id="services"> 
+          <img src="images/equipe.png" alt="tim?" title="tim?" width="960" height="104">
+      </section>
+    </div>
+    <!-- main content -->
+    <div id="content">
+
+      <div id="conteneurmatieres">
+      <h1>L'équipe enseignante</h1>
+        <div id="matieres">
+
+            <?php include('admin/connectionbdd.php'); 
+            $sql ="SELECT * FROM prof ORDER BY prenom ASC";
+            $liste = $pdo->query($sql);
+            while ($data = $liste->fetch()) {
+              echo "<div class='enseignants'>";
+              echo "<img src='admin/".$data['image']."' alt='".$data['prenom']."' width='170' height='200'/>";
+              echo "<h2>".$data['prenom']." ".$data['nom']."</h2>";
+              echo "<ul>";
+              echo "<li>".$data['role']."</li>";
+              echo "<li><a href='mailto:".$data['courriel']."'>Courriel</a></li>";
+              echo "<li> Poste :".$data['tel_poste']."</li>";
+              echo "<li> Bureau :".$data['bureau']."</li>";
+              echo "<li> Site Web : <br/ > <a href='".$data['site']."'target='_blank'>".$data['courriel']."</a></li>";
+              echo "</ul>";
+              echo "</div>";
+            }
+            ?>    
+          </div>
+        </div>    
+          <div class="clear"></div>
+          <div id="citations">
+            <p class="cita">"Une formation qui a de l'avenir..."</p>
+          </div><!-- FIN CITATIONS -->
+        
+      
+    </div><!--FIN content -->
+  
+    <div class="clear">
+
+    <?php include("morceaux/footer.php") ?>
+</div>
+</body>
+</html>
