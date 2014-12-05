eeb4e90a6205435ed660a88150575b306c32c3b6
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-05 15:38:08 -0500

    Sécurité pour la partie admin

--- admin/header-admin.php
+++ admin/header-admin.php
@@ -1,3 +1,27 @@
+<?php 
+	$token = '';
+	if(isset($_COOKIE['token']) && !empty($_COOKIE['token'])) {
+		include_once "connectionbdd.php";
+		$token = $_COOKIE['token']; # Récupération des Cookies & Ou Sessions
+		$login_q = $pdo->prepare("SELECT * FROM admin WHERE jeton = :token");
+		$login_q->execute(array(
+			'token' => $token
+		));
+		
+		$user = $login_q->fetch(PDO::FETCH_OBJ);
+		
+		if(!$user){
+			header('Location: index.php'); # Redirection
+			exit();
+		}else{
+			include("menu-admin.php");
+		}
+	} else {
+		header('Location: index.php'); # Redirection
+		exit();	
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,9 +34,5 @@
 	<title>#TIM Matane - Administration Actualités</title>
 </head>
 <body>
-<?php 
-	include("menu-admin.php");
-	include_once "connectionbdd.php";
-?>
 <div class="right">
 <section>
