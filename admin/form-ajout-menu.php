b23dbfa89e3c55e255fcc3cbe3087a1616d3df6e
DumontKevin <Dumont.Kevin@cgmatane.qc.ca>
2014-12-08 21:49:44 -0500

    Update form-ajout-menu.php

--- admin/form-ajout-menu.php
+++ admin/form-ajout-menu.php
@@ -15,10 +15,29 @@
 				</div>
 				
 				<div>
-					<label for="nom_lien">Lien du menu principal</label>
-					<input type="text" id="test" name="nom_lien" />
+				<?php
+					//Requete à la BD pour PAGE
+					$sql="SELECT * FROM page";
+					$req = $pdo->query($sql);
+					
+					//Fait apparaître le select
+					echo "<p class='border-deroulant'><label for='nom_lien'>Lien du menu principal</label><select class='select' name='nom_lien'>";
+					
+					//Choix vide
+					echo "<option value=''></option>";
+					
+					//Affiche comme option: Les pages; Le value du POST = Id de la page
+					while($data = $req->fetch()){
+						echo "<option value='".$data['page_id']."'>".$data['titre']."</option>";
+					}
+					
+					echo "</select> </p> ";
+				?>
 				</div>
 			</div>
+			
+			<!-- Boutton permettant d'appeler une fonction AJAX dans script.js -->
+			<!-- Permet l'ajout de sous-menu -->
 			<a href="#" onclick="javascript:ajoutSM('menu')" >Ajouter un sous-menu</a>
 		</fieldset>
 		
