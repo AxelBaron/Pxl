2194c2ecedd056f2dda175e3f764f284b235110d
Skyzofraise <to-x-icdoll@hotmail.fr>
2014-12-10 14:36:54 -0500

    favicon

---
+++ morceaux/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="Content-Type" content="UTF-8">
+    <meta name="Content-Language" content="fr">
+    <meta name="Keywords" content="Matane, Cégep, TIM, tim, #TIM, #tim, techniques d'intégration multimédia, multimédia, web, intégration, développement, graphisme, vidéo, jeu vidéo, animateur, concepteur, programmeur, intégrateur, Canada, Québec">
+    <meta name="Subject" content="Département TIM du Cégep de Matane">
+    <meta name="Copyright" content="Finissants de TIM">
+    <meta name="Author" content="Baron Axel, Dumont Kévin, Grasset Lolita, Perron Antoine">
+    <meta name="Rating" content="general">
+    <meta name="Distribution" content="global">
+    <meta name="Geography" content="Matane, Canada, G4W 1L1">
+    <meta name="Category" content="internet">
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="screen" href="style.css" type="text/css">
+    <script type="text/javascript">
+    sfHover = function() {
+            var sfEls = document.getElementById("header").getElementsByTagName("LI");
+            for (var i=0; i<sfEls.length; i++) {
+                    sfEls[i].onmouseover=function() {
+                            this.className+=" sfhover";
+                    }
+                    sfEls[i].onmouseout=function() {
+                            this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+                    }
+            }
+    }
+    if (window.attachEvent) window.attachEvent("onload", sfHover);
+    </script>
+
+    
+    <link rel="shortcut icon" href="images/favicon.ico"> 
+    <script src="js/modernizr.custom.js"></script>
+
+  
